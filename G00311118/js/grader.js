e5505b25272621d09944b52bb91904d17d1146ff
G003111118 <35809339+G003111118@users.noreply.github.com>
2018-04-19 16:58:12 +0100

    Add files via upload
    irish complete

--- G00311118/js/grader.js
+++ G00311118/js/grader.js
@@ -1,33 +1,68 @@
 function gradeResult() {
-	var myResult = parseInt(document.getElementById("value1").value);
-	var grade;
+	var myEnglishResult = parseInt(document.getElementById("value-english").value);
+	var myIrishResult = parseInt(document.getElementById("value-irish").value);
+	var englishGrade, irishGrade;
 	
-	if (!myResult){
+	if (!myEnglishResult){
 		alert("Please enter a result");
 	}
-	else if (myResult <0 || myResult >100){
+	else if (myEnglishResult <0 || myEnglishResult >100){
 		alert("Please eneter a number between 0 and 100");
 	}
 	
 	else{
-		if (myResult > 90) {
-			grade = 'A';
-		}else if (myResult > 80) {
-			grade = 'B';
-		}else if (myResult > 70) {
-			grade = 'B-';
-		}else if (myResult > 60) {
-			grade = 'C';
-		}else if (myResult > 50) {
-			grade = 'C-';
-		}else if (myResult > 40) {
-			grade = 'D';
+		if (myEnglishResult > 90) {
+			englishGrade= 'A';
+		}else if (myEnglishResult > 80) {
+			englishGrade = 'B';
+		}else if (myEnglishResult > 70) {
+			englishGrade = 'B-';
+		}else if (myEnglishResult > 60) {
+			englishGrade = 'C';
+		}else if (myEnglishResult > 50) {
+			englishGrade = 'C-';
+		}else if (myEnglishResult > 40) {
+			englishGrade = 'D';
 		}else {
-			grade = 'F';
+			englishGrade = 'F';
 		}
 		
-		var message = 'Your result of ' + myResult + ' gets you a grade ' + grade;
+		var message = 'Your English result of ' + myEnglishResult + ' gets you a grade ' + englishGrade;
 		console.log(message);
-		document.getElementById("resultsentence").innerHTML = message;
+		document.getElementById("Englishresultsentence").innerHTML = message;
+	}
+		
+		
+		
+		if (!myIrishResult){
+		alert("Please enter a result");
+	}
+	else if (myIrishResult <0 || myIrishResult >100){
+		alert("Please eneter a number between 0 and 100");
+	}
+	
+	else{
+		if (myIrishResult > 90) {
+			Irishgrade= 'A';
+		}else if (myIrishResult > 80) {
+			Irishgrade = 'B';
+		}else if (myIrishResult > 70) {
+			Irishgrade = 'B-';
+		}else if (myIrishResult > 60) {
+			Irishgrade = 'C';
+		}else if (myIrishResult > 50) {
+			Irishgrade = 'C-';
+		}else if (myIrishResult > 40) {
+			Irishgrade = 'D';
+		}else {
+			Irishgrade = 'F';
+		}
+		
+		
+		
+		
+		var message = 'Your Irish result of ' + myIrishResult + ' gets you a grade ' + Irishgrade;
+		console.log(message);
+		document.getElementById("Irishresultsentence").innerHTML = message;
 	}
 }
